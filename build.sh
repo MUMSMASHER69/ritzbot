e6883479d85b5c011560dc6a9c896c0849ba8710
lukekoko <lukekoko1@gmail.com>
2021-02-08 12:44:09 +0000

    Init

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+sudo docker stop ritz
+
+sudo docker rm ritz
+
+sudo docker build -t ritzbot .
+
+sudo docker run \
+--name ritz \
+--mount type=bind,source=/home/luke/projects/discordbot/logs,target=/usr/src/app/logs/ \
+--restart=always \
+ritzbot
--- ritz.py
+++ ritz.py
@@ -2,9 +2,21 @@ import discord
 import re
 import json
 import random
+from random import shuffle
+import logging
+import logging.config
+
+logging.config.fileConfig(
+    "./config/logger.ini", disable_existing_loggers=False
+)
+logger = logging.getLogger(__name__)
 
 with open("config/config.json", 'r') as f:
     config = json.load(f)
+
+with open("config/token.json", 'r') as f:
+    tokenfile = json.load(f)
+    
 
 searchList = [    
     r"\$(\d+\.?[0-9]?[0-9]?)",
@@ -22,6 +34,9 @@ class MyClient(discord.Client):
         if (message.author == self.user):
             return
 
+        # if (str(message.author) == "cookedswag#5260"):
+        #     return
+
         # dollar to ritz conversion
         for i in searchList:
             found = re.search(i, message.content)
@@ -29,23 +44,27 @@ class MyClient(discord.Client):
                 ritzAmount = float(found.group(1)) / ritzprice
                 if (ritzAmount.is_integer()):
                     ritzAmount = int(ritzAmount)
-                ritzString = "{} Ritz".format(ritzAmount)
+                ritzString = "{:.2f} Ritz at ${:.2f} per box".format(ritzAmount, ritzprice)
                 await message.channel.send(str(ritzString), file=discord.File("./images/ritz.jpeg"))
                 return
 
         # word responses
-        for i in config["words"]:
+        wordList = config["words"]
+        shuffle(wordList)
+        for i in wordList:
             if (i["word"] in message.content.lower()):
                 if (i["type"] == "link"):
                     await message.channel.send(random.choice(i["content"]))
                     return
                 elif (i["type"] == "image"):
-                    await message.channel.send(file=discord.File(random.choice(i["content"])))
+                    await message.channel.send(file=discord.File("./images/" + random.choice(i["content"])))
                     return
 
         if ("!help" == message.content.lower()):
             words = ""
-            for i in config["words"]:
+            wordList = config["words"]
+            shuffle(wordList)
+            for i in wordList:
                 words += "\t" + i["word"] + "\n"
 
             with open("help.txt", 'r') as f:
@@ -60,23 +79,23 @@ class MyClient(discord.Client):
 
         # random
         rare_num = random.randint(0,100)
-        super_rare_num = random.randint(0, 1000)
-        ultra_rare_num = random.randint(0, 10000)
-        if (ultra_rare_num == 69):
-            print("ultra rare")
-            choice = config["ultra_rare"]
-            await message.channel.send(file=discord.File("./images/" + random.choice(choice)))
+        epic_num = random.randint(0, 1000)
+        legendary_num = random.randint(0, 10000)
+        if (legendary_num == 69):
+            logger.info("Legendary")
+            choice = config["legendary"]
+            await message.channel.send("Legendary. Just vibin' in the ritz car.", file=discord.File("./images/" + random.choice(choice)))
             return
-        elif (super_rare_num == 69):
-            print("super rare")
-            choice = config["super_rare"]
-            await message.channel.send(file=discord.File("./images/" + random.choice(choice)))
+        elif (epic_num == 69):
+            logger.info("Epic")
+            choice = config["epic"]
+            await message.channel.send("Epic. What.", file=discord.File("./images/" + random.choice(choice)))
             return
-        elif (rare_num == 69):
-            print('yo')
-            choice = config["rare"]
-            await message.channel.send(file=discord.File("./images/" + random.choice(choice)))
-            return
+        # elif (rare_num == 69):
+        #     logger.info("Rare")
+        #     choice = config["rare"]
+        #     await message.channel.send("Rare.", file=discord.File("./images/" + random.choice(choice)))
+        #     return
 
         # darren
         if (str(message.author) == "mckhira#3664"):
@@ -96,7 +115,7 @@ def loadConfig():
 
 def main():
     client = MyClient()
-    client.run(config["token"])
+    client.run(tokenfile["token"])
 
 
 if __name__ == "__main__":
